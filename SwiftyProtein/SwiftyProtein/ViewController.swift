5fb45473419eab7b03f123b177504b2782aa9033
stevevovchyna <steve.vovchyna@gmail.com>
2019-12-23 01:17:48 +0200

    init

---
+++ SwiftyProtein/SwiftyProtein/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyProtein
+//
+//  Created by Steve Vovchyna on 23.12.2019.
+//  Copyright © 2019 Steve Vovchyna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
