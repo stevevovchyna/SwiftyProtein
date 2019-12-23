4bb281028041577aca6f8082890a441a49e26a09
stevevovchyna <steve.vovchyna@gmail.com>
2019-12-23 23:38:41 +0200

    ligands are now displayed with connections, colors etc.

--- SwiftyProtein/SwiftyProtein/Controller/ProteinViewController.swift
+++ SwiftyProtein/SwiftyProtein/Controller/ProteinViewController.swift
@@ -49,11 +49,106 @@ class Atoms {
                 sphere.firstMaterial?.diffuse.contents = UIColor.systemPink
             }
             let sphereNode = SCNNode(geometry: sphere)
-            sphereNode.position = SCNVector3(x: atom.model_Cartn_x, y: atom.model_Cartn_y, z: atom.model_Cartn_z)
+            sphereNode.position = SCNVector3(x: atom.xPos, y: atom.yPos, z: atom.zPoz)
             liganeMolecule.addChildNode(sphereNode)
+        }
+        for bonds in ligand.allConects {
+            for bond in bonds.bondAtoms {
+                let newBond = SCNNode()
+                let startAtom = ligand.allAtoms[bonds.id - 1]
+                let endAtom = ligand.allAtoms[bond - 1]
+                let startVector = SCNVector3(x: startAtom.xPos, y: startAtom.yPos, z: startAtom.zPoz)
+                let endVector = SCNVector3(x: endAtom.xPos, y: endAtom.yPos, z: endAtom.zPoz)
+                liganeMolecule.addChildNode(newBond.buildConect(from: startVector, to: endVector, radius: 0.05, color: .black))
+            }
         }
         return liganeMolecule
     }
+}
+
+extension SCNNode {
+
+        func normalizeVector(_ iv: SCNVector3) -> SCNVector3 {
+            let length = sqrt(iv.x * iv.x + iv.y * iv.y + iv.z * iv.z)
+            if length == 0 {
+                return SCNVector3(0.0, 0.0, 0.0)
+            }
+
+            return SCNVector3( iv.x / length, iv.y / length, iv.z / length)
+
+        }
+        
+        func buildConect(from startPoint: SCNVector3,
+                                  to endPoint: SCNVector3,
+                                  radius: CGFloat,
+                                  color: UIColor) -> SCNNode {
+            let w = SCNVector3(x: endPoint.x-startPoint.x,
+                               y: endPoint.y-startPoint.y,
+                               z: endPoint.z-startPoint.z)
+            let l = CGFloat(sqrt(w.x * w.x + w.y * w.y + w.z * w.z))
+
+            if l == 0.0 {
+                // two points together.
+                let sphere = SCNSphere(radius: radius)
+                sphere.firstMaterial?.diffuse.contents = color
+                self.geometry = sphere
+                self.position = startPoint
+                return self
+
+            }
+
+            let cyl = SCNCylinder(radius: radius, height: l)
+            cyl.firstMaterial?.diffuse.contents = color
+
+            self.geometry = cyl
+
+            //original vector of cylinder above 0,0,0
+            let ov = SCNVector3(0, l/2.0,0)
+            //target vector, in new coordination
+            let nv = SCNVector3((endPoint.x - startPoint.x)/2.0, (endPoint.y - startPoint.y)/2.0,
+                                (endPoint.z-startPoint.z)/2.0)
+
+            // axis between two vector
+            let av = SCNVector3( (ov.x + nv.x)/2.0, (ov.y+nv.y)/2.0, (ov.z+nv.z)/2.0)
+
+            //normalized axis vector
+            let av_normalized = normalizeVector(av)
+            let q0 = Float(0.0) //cos(angel/2), angle is always 180 or M_PI
+            let q1 = Float(av_normalized.x) // x' * sin(angle/2)
+            let q2 = Float(av_normalized.y) // y' * sin(angle/2)
+            let q3 = Float(av_normalized.z) // z' * sin(angle/2)
+
+            let r_m11 = q0 * q0 + q1 * q1 - q2 * q2 - q3 * q3
+            let r_m12 = 2 * q1 * q2 + 2 * q0 * q3
+            let r_m13 = 2 * q1 * q3 - 2 * q0 * q2
+            let r_m21 = 2 * q1 * q2 - 2 * q0 * q3
+            let r_m22 = q0 * q0 - q1 * q1 + q2 * q2 - q3 * q3
+            let r_m23 = 2 * q2 * q3 + 2 * q0 * q1
+            let r_m31 = 2 * q1 * q3 + 2 * q0 * q2
+            let r_m32 = 2 * q2 * q3 - 2 * q0 * q1
+            let r_m33 = q0 * q0 - q1 * q1 - q2 * q2 + q3 * q3
+
+            self.transform.m11 = r_m11
+            self.transform.m12 = r_m12
+            self.transform.m13 = r_m13
+            self.transform.m14 = 0.0
+
+            self.transform.m21 = r_m21
+            self.transform.m22 = r_m22
+            self.transform.m23 = r_m23
+            self.transform.m24 = 0.0
+
+            self.transform.m31 = r_m31
+            self.transform.m32 = r_m32
+            self.transform.m33 = r_m33
+            self.transform.m34 = 0.0
+
+            self.transform.m41 = (startPoint.x + endPoint.x) / 2.0
+            self.transform.m42 = (startPoint.y + endPoint.y) / 2.0
+            self.transform.m43 = (startPoint.z + endPoint.z) / 2.0
+            self.transform.m44 = 1.0
+            return self
+        }
 }
 
 class ProteinViewController: UIViewController {
@@ -65,8 +160,6 @@ class ProteinViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let lig = ligandToDisplay else { return }
-        
         sceneSetup()
         geometryNode = Atoms.addLigandAtoms(ligand: ligandToDisplay!)
         sceneView.scene!.rootNode.addChildNode(geometryNode)
--- SwiftyProtein/SwiftyProtein/Model/Ligand.swift
+++ SwiftyProtein/SwiftyProtein/Model/Ligand.swift
@@ -10,99 +10,60 @@ import Foundation
 import UIKit
 
 struct Atom {
-    let id: String
+    let id: Int
     let atom_name: String
-//    let label_atom_id: String
-//    let charge: Double
-//    let pdbx_align: Double
-//    let pdbx_aromatic_flag: String
-//    let pdbx_leaving_atom_flag: String
-//    let pdbx_stereo_config: String
-    let model_Cartn_x: Float
-    let model_Cartn_y: Float
-    let model_Cartn_z: Float
-//    let pdbx_model_Cartn_x_ideal: Double
-//    let pdbx_model_Cartn_y_ideal: Double
-//    let pdbx_model_Cartn_z_ideal: Double
-//    let pdbx_component_atom_id: String
-//    let pdbx_component_comp_id: String
-//    let pdbx_ordinal: String
+    let xPos: Float
+    let yPos: Float
+    let zPoz: Float
 
-    init(atomData: [String]) {
-        id = atomData[1] == "" ? "N" : atomData[1]
-        
-        
-//        charge = atomData[2] == "" ? 0 : (atomData[2] as NSString).doubleValue
-//        pdbx_align = atomData[3] == "" ? 0 : (atomData[3] as NSString).doubleValue
-//        pdbx_aromatic_flag = atomData[4] == "" ? "N" : atomData[4]
-//        pdbx_leaving_atom_flag = atomData[5] == "" ? "N" : atomData[5]
-//        pdbx_stereo_config = atomData[6] == "" ? "N" : atomData[6]
-        
-        model_Cartn_x = atomData[6] == "" ? 0 : (atomData[6] as NSString).floatValue
-        model_Cartn_y = atomData[7] == "" ? 0 : (atomData[7] as NSString).floatValue
-        model_Cartn_z = atomData[8] == "" ? 0 : (atomData[8] as NSString).floatValue
-        
-        atom_name = atomData[11] == "" ? "N" : atomData[11]
-//        pdbx_model_Cartn_x_ideal = atomData[10] == "" ? 0 : (atomData[10] as NSString).doubleValue
-//        pdbx_model_Cartn_y_ideal = atomData[11] == "" ? 0 : (atomData[11] as NSString).doubleValue
-//        pdbx_model_Cartn_z_ideal = atomData[12] == "" ? 0 : (atomData[12] as NSString).doubleValue
-//        pdbx_component_atom_id = atomData[13] == "" ? "N" : atomData[13]
-//        pdbx_component_comp_id = atomData[14] == "" ? "N" : atomData[14]
-//        pdbx_ordinal = atomData[15] == "" ? "N" : atomData[15]
+    init(atomData: String) {
+        id = (atomData[atomData.index(atomData.startIndex, offsetBy: 6)...atomData.index(atomData.startIndex, offsetBy: 10)] as NSString).integerValue
+        atom_name = String(atomData[atomData.index(atomData.startIndex, offsetBy: 76)...atomData.index(atomData.startIndex, offsetBy: 77)]).trimmingCharacters(in: .whitespacesAndNewlines)
+        xPos = (atomData[atomData.index(atomData.startIndex, offsetBy: 30)...atomData.index(atomData.startIndex, offsetBy: 37)] as NSString).floatValue
+        yPos = (atomData[atomData.index(atomData.startIndex, offsetBy: 38)...atomData.index(atomData.startIndex, offsetBy: 45)] as NSString).floatValue
+        zPoz = (atomData[atomData.index(atomData.startIndex, offsetBy: 47)...atomData.index(atomData.startIndex, offsetBy: 54)] as NSString).floatValue
     }
 }
 
-//struct Conect {
-//    let alt_atom_id: String
-//    let type_symbol: String
-//    let pdbx_aromatic_flag: String
-//    let pdbx_leaving_atom_flag: String
-//    let pdbx_stereo_config: String
-//    let model_Cartn_x: Double
-//    let model_Cartn_y: Double
-//    let model_Cartn_z: Double
-//    let pdbx_model_Cartn_x_ideal: Double
-//    let pdbx_model_Cartn_y_ideal: Double
-//    let pdbx_model_Cartn_z_ideal: Double
-//    let pdbx_component_atom_id: String
-//    let pdbx_component_comp_id: String
-//    let pdbx_ordinal: String
-//
-//    init(atomData: [String]) {
-//        alt_atom_id = atomData[0] == "" ? "N" : atomData[0]
-//        type_symbol = atomData[1] == "" ? "N" : atomData[2]
-//        pdbx_aromatic_flag = atomData[4] == "" ? "N" : atomData[4]
-//        pdbx_leaving_atom_flag = atomData[5] == "" ? "N" : atomData[5]
-//        pdbx_stereo_config = atomData[6] == "" ? "N" : atomData[6]
-//        model_Cartn_x = atomData[7] == "" ? 0 : (atomData[7] as NSString).doubleValue
-//        model_Cartn_y = atomData[8] == "" ? 0 : (atomData[8] as NSString).doubleValue
-//        model_Cartn_z = atomData[9] == "" ? 0 : (atomData[9] as NSString).doubleValue
-//        pdbx_model_Cartn_x_ideal = atomData[10] == "" ? 0 : (atomData[10] as NSString).doubleValue
-//        pdbx_model_Cartn_y_ideal = atomData[11] == "" ? 0 : (atomData[11] as NSString).doubleValue
-//        pdbx_model_Cartn_z_ideal = atomData[12] == "" ? 0 : (atomData[12] as NSString).doubleValue
-//        pdbx_component_atom_id = atomData[13] == "" ? "N" : atomData[13]
-//        pdbx_component_comp_id = atomData[14] == "" ? "N" : atomData[14]
-//        pdbx_ordinal = atomData[15] == "" ? "N" : atomData[15]
-//    }
-//}
+struct Conect {
+
+    let id: Int
+    var bondAtoms: [Int]
+    
+    init(conectData data: String) {
+        id = (data[data.index(data.startIndex, offsetBy: 6)...data.index(data.startIndex, offsetBy: 10)] as NSString).integerValue
+        let bondsString = String(data[data.index(data.startIndex, offsetBy: 11)...data.index(before: data.endIndex)])
+        var bondsArray = bondsString.components(separatedBy: " ")
+        bondsArray = bondsArray.filter{ $0 != "" }
+        bondAtoms = []
+        for bond in bondsArray {
+            self.bondAtoms.append((bond as NSString).integerValue)
+        }
+    }
+}
 
 class Ligand {
     let name: String
     var allAtoms: [Atom] = []
-//    let allConects: [Conect]
+    var allConects: [Conect] = []
     
     init(forLigand ligand: String, withDataSet data: String) {
+
         let stringsArray = data.components(separatedBy: "\n")
-        var atoms : [[String]] = []
-        for line in stringsArray {
-            var atom = line.components(separatedBy: " ")
-            atom = atom.filter{ $0 != "" }
-            atoms.append(atom)
-        }
-        for atom in atoms {
-            if atom.contains("ATOM") {
-                allAtoms.append(Atom(atomData: atom))
+        var atomStrings: [String] = []
+        var conectStrings: [String] = []
+        for entry in stringsArray {
+            if entry.starts(with: "ATOM") {
+                atomStrings.append(entry)
+            } else if entry.starts(with: "CONECT") {
+                conectStrings.append(entry)
             }
+        }
+        for atom in atomStrings {
+            allAtoms.append(Atom(atomData: atom))
+        }
+        for conect in conectStrings {
+            allConects.append(Conect(conectData: conect))
         }
         name = ligand
     }
