31d64472176fd88e4facbb6f7e7246ab88bcf18e
stevevovchyna <steve.vovchyna@gmail.com>
2020-01-10 19:51:33 +0200

    photo library access request added

--- SwiftyProtein/SwiftyProtein/View/LigandInfoTableViewCell.swift
+++ SwiftyProtein/SwiftyProtein/View/LigandInfoTableViewCell.swift
@@ -42,13 +42,13 @@ class LigandInfoTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        let labelsArray = [ligandNameLabel, identifiersLabel, formulaLabel, molecularWeightLabel, typeLabel, isometricSmilesLabel, InChlLabel, InChIKeyLabel, formalChargeLabel, atomCountLabel, chiralAtomsLabel, chiralAtomCountLabel, boundCountLabel, aromaticBoundCountLabel]
-        getRoundedBorders(forLabels: labelsArray as! [UILabel])
+        let labelsArray: [UILabel] = [ligandNameLabel, identifiersLabel, formulaLabel, molecularWeightLabel, typeLabel, isometricSmilesLabel, InChlLabel, InChIKeyLabel, formalChargeLabel, atomCountLabel, chiralAtomsLabel, chiralAtomCountLabel, boundCountLabel, aromaticBoundCountLabel]
+        getRoundedBorders(forLabels: labelsArray, with: .gray)
     }
     
-    func getRoundedBorders(forLabels labels: [UILabel]) {
+    func getRoundedBorders(forLabels labels: [UILabel], with color: UIColor) {
         for label in labels {
-            label.backgroundColor = .gray
+            label.backgroundColor = color
             label.layer.masksToBounds = true
             label.layer.cornerRadius = 5
         }
